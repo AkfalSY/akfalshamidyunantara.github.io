6780f3615bbc2463ceedc264f00cbdbe3f212a12
AkfalSY <akfaltara@upi.edu>
2025-07-09 10:32:23 +0700

    update pertama

--- script.js
+++ script.js
@@ -29,3 +29,26 @@
         clickable: true,
       },
     }); 
+
+   //  Copy Email 
+   const email = "akfaltara@upi.edu";
+
+      function copyEmail(button) {
+    const textSpan = button.querySelector(".copy-text");
+
+    navigator.clipboard.writeText(email).then(() => {
+      const originalText = textSpan.textContent;
+      textSpan.textContent = "Copied!";
+
+      // Tambahkan animasi (opsional)
+      button.classList.add("copied");
+
+      // Kembalikan ke semula setelah 1 detik
+      setTimeout(() => {
+        textSpan.textContent = originalText;
+        button.classList.remove("copied");
+      }, 1000);
+    }).catch(err => {
+      alert("Failed to copy: " + err);
+    });
+  }
